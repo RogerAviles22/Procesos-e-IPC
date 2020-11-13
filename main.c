a8a75b38202d46d527f81b42c9b881973fd7110f
Roger Aviles <rogiavil@espol.edu.ec>
2020-11-13 10:36:59 -0500

    Fin de Shell

--- main.c
+++ main.c
@@ -5,61 +5,71 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define BUFFER_LEN 1024
+#define LEN_ENTRADA 1024
 
 int main(){
 
-   char* argv[100];        //user command
-   char* path= "/bin/";    //set path at bin
-   //int argc;               //arg count
-   char progpath[20];      //full file path
-   char entrada[BUFFER_LEN];  //get command entrada
+   char *argv[150];        //Almacena los comandos
+   char cmd_ruta[25];      //Almacena el comando y la carpeta /bin ejecutables
+   char entrada[LEN_ENTRADA];  //Almacena lo que escribe el usuario
+   int argc=0;
 
 	while(1){
 
-	    printf("Ingrese el comando>> ");                    //print shell prompt
+		//Entra en el bucle en caso que el usuario presione Enter o no escriba un comando
+		do{
+			printf("Ingrese el comando >> ");     
 
-	    if(!fgets(entrada, BUFFER_LEN, stdin))
-	   		break;
+			//Recibe los datos escritos en shell
+		    if(!fgets(entrada, LEN_ENTRADA, stdin)){
+		    	return -1;	    
+		    }
 
-		size_t length = strlen(entrada);
-		if (entrada[length - 1] == '\n')
-	   		entrada[length - 1] = '\0';
+		    //Cambiamos el salto de linea por NULL caracter
+			size_t length = strlen(entrada);
+			if (entrada[length - 1] == '\n')
+		   		entrada[length - 1] = '\0';
 
-	   	if(strcmp(entrada, "exit")==0)           //check if command is exit
-	       break;
-	   
 
-	   char *token;                  //split command into separate strings
-	   token = strtok(entrada," ");
-	   int i=0;
-	   while(token!=NULL){
-	       argv[i]=token;      
-	       token = strtok(NULL," ");
-	       i++;
-	   }
-	   argv[i]=NULL;                     //set last value to NULL for execvp
+		   	//Salida del shell-personalizado
+		   	if(strcmp(entrada, "exit")==0)
+		   		return 0;       		   	           
+		   
+		   	//Dividimos el comando por el caracter espacio " "
+		   char *token;              
+		   token = strtok(entrada," ");
+		   int i=0;
+		   while(token!=NULL){
+		       argv[i]=token;      
+		       token = strtok(NULL," ");
+		       i++;
+		   }
+		   argv[i]=NULL;                 
+		   argc=i;
 
-	   //argc=i;                           //get arg count
-	   //for(i=0; i<argc; i++)
-	   //    printf("%s\n", argv[i]);         //print command/args
-	   
-	   strcpy(progpath, path);              //copy /bin/ to file path
-	   strcat(progpath, argv[0]);           //add program to path
+		   //Validamos que haya al menos 1 comando escrito
+		   if(i==0){
+		       fprintf(stderr, "Escriba más de 1 comando!\n");	 
+		   }
 
-	   for(i=0; i<strlen(progpath); i++){   //delete newentrada
-	       if(progpath[i]=='\n'){      
-	           progpath[i]='\0';
+		}while(argc==0);
+
+	   //copiamos /bin
+	   strcpy(cmd_ruta, "/bin/");  
+	   //adjuntamos comandos            
+	   strcat(cmd_ruta, argv[0]);              
+
+	   for(int i=0; i<strlen(cmd_ruta); i++){   
+	       if(cmd_ruta[i]=='\n'){      
+	           cmd_ruta[i]='\0';
 	       }
 	   }
-	   //int pid= fork();           //fork child
 
-	   /*if(pid==0)*/
-	   if(fork()==0){                //Child
-	       execvp(progpath,argv);
-	       fprintf(stderr, "¡Invalid command!\n");
+	   if(fork()==0){                
+	       execvp(cmd_ruta,argv);
+	       fprintf(stderr, "¡Comando Inválido!\n");
 
-	   }else{                     //Parent
+	   }else{                     
 	       wait(NULL);
 	       printf("OK!\n");
 	   }
